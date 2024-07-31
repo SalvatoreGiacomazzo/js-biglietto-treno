a1263e082d5b698f9235284590a663b9d56540f4
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-07-31 16:07:22 +0200

    written to do list

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK')
+
+// Calcolo del prezzo di un biglietto del treno 
+
+/*Dovremo chiedere all'utente il numero dei chilometri che intende percorrere e la sua età.
+L'obiettivo è calcolare il prezzo del biglietto del treno valutando che: il prezzo del biglietto è €0.21 per Km percorso, 
+se l'utente ha meno di 18 anni sarà applicato uno sconto del 20%;
+se l'utente ha più di 65 anni sarà applicato uno sconto del 40%.
+
+
+
+/*TO DO LIST
+1. Acquisizione dell'età dell'utente.
+2. Acquisizione del numero di km che l'utente intende percorrere.
+3. Calcolo del prezzo.
+4. Applicazione di eventuale sconto.
+5. Mostrare il risultato a schermo.
+*/ 
